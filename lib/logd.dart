fa0dc9f4befb2e96617b0e6c118986b2403e8bc1
PooriaAskariM <pooriaaskarim@gmail.com>
2025-11-06 14:25:41 +0330

    initial commit:   Package library exports.

--- lib/logd.dart
+++ lib/logd.dart
@@ -2,4 +2,5 @@ library;
 
 export 'src/logger/logger.dart';
 export 'src/printer/box.printer.dart';
+export 'src/stack_trace_parser/stack_trace_parser.dart';
 export 'src/time/timestamp_formatter.dart';
